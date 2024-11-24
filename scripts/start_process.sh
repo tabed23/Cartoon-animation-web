eac384084421f42ea9147e8d129502df2c620395
Ubuntu <ubuntu@ip-172-31-91-112.ec2.internal>
2024-11-24 20:04:38 +0000

    for my docker application

---
+++ scripts/start_process.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Login to Amazon ECR in ap-southeast-1 region
+sudo -u root aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 823712965653.dkr.ecr.us-east-1.amazonaws.com
+
+# Sleep for 3 seconds to ensure clean-up is complete
+sleep 3
+
+# Pull the latest image from ECR for jewel-cartoon-repo
+sudo -u root docker pull 823712965653.dkr.ecr.us-east-1.amazonaws.com/jewel-cartoon-repo:latest
+
+# If the pull command fails, try logging in again and pulling the image
+if [[ $? != 0 ]]; then
+    sudo -u root aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 823712965653.dkr.ecr.us-east-1.amazonaws.com
+    sudo -u root docker pull 823712965653.dkr.ecr.us-east-1.amazonaws.com/jewel-cartoon-repo:latest
+fi
+
+# Run the Docker container with the pulled image
+sudo -u root docker run -d -p 3000:3000 --name jewel-cartoon-repo 823712965653.dkr.ecr.us-east-1.amazonaws.com/jewel-cartoon-repo:latest
+
